56665ca9810d5d457d6df0af29a44c2fd2cdf3ce
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2024-06-25 09:34:03 +0100

    Update paramspackager.py

--- paramspackager.py
+++ paramspackager.py
@@ -6,6 +6,6 @@ import glob
 directory = input("Enter Directory Path: ")
 for file in glob.glob(os.path.join(directory, '*.txt')):
     with open(file, 'r') as f:
-        str = [i.replace("\n","") for i in f.readlines()]
-    with open(file.split(".")[0]+".txt","w") as f:
+        str_ = "n".join([i.replace("\n","") for i in f.readlines()])
+    with open(file,"w") as f:
         f.write(str_)
