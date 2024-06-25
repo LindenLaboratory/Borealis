2f994cd4527ced0a4649da9b6100f7450794a154
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2024-06-25 09:34:26 +0100

    Update paramspackager.py

--- paramspackager.py
+++ paramspackager.py
@@ -7,5 +7,7 @@ directory = input("Enter Directory Path: ")
 for file in glob.glob(os.path.join(directory, '*.txt')):
     with open(file, 'r') as f:
         str_ = "n".join([i.replace("\n","") for i in f.readlines()])
+        f.close()
     with open(file,"w") as f:
         f.write(str_)
+        f.close()
