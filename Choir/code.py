32d0c6e6eda32c2e8df3e11f62ba4b0509250310
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2024-07-23 11:58:01 +0100

    Update code.py

--- Choir/code.py
+++ Choir/code.py
@@ -13,6 +13,7 @@ import adafruit_requests
 import json
 #SETUP
 kbd = Keyboard(usb_hid.devices)
+HOST,PORT="192.168.4.1",80
 layout = KeyboardLayoutUS(kbd)
 lst = ["D:","E:","F:","G:","H:","I:","J:"]
 led = digitalio.DigitalInOut(board.LED)
@@ -22,14 +23,14 @@ with open("Borealis/settings.txt") as f:
     mode = str(f.readlines()[6]).replace("\n","").strip()
 #FUNCTIONS
 def hid():
-    time.sleep(1)
+    time.sleep(1.25)
     kbd.send(Keycode.WINDOWS,Keycode.R)
-    time.sleep(0.125)
-    layout.write('powershell\n')
-    time.sleep(1)
-    layout.write("(Get-WmiObject -Namespace root/wmi -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1, 0)\n")
-    layout.write(f'{";".join(lst)}\n')
-    layout.write("taskkill /f /im pythonw.exe;./run.bat; timeout /t 1; taskkill /F /IM cmd.exe; (Get-WmiObject -Namespace root/wmi -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1, 100); taskkill /F /IM powershell.exe\n")
+    time.sleep(0.5)
+    layout.write('cmd\n')
+    time.sleep(1.75)
+    for i in lst: layout.write(f'{i}\n')
+    layout.write("taskkill /f /im pythonw.exe\n")
+    layout.write("run.bat && timeout /t 3 && taskkill /F /IM cmd.exe\n")
     while True: led.value = True
 def send(jsondata=None):
     wifi.radio.connect("Borealis", "pico-pico")
@@ -40,12 +41,18 @@ def send(jsondata=None):
     }
     f = open('data.json',encoding='utf-8')
     if jsondata:
-        response = requests.post("http://192.168.4.1/", json=jsondata, headers=request_header)
+        requests.post("http://192.168.4.1/", json=jsondata, headers=request_header)
     else:
         data = json.load(f)
         data["log"] = ">:" + data["log"] + ":<"
         f.close()
-        response = requests.post("http://192.168.4.1/", json=data, headers=request_header)
+        requests.post("http://192.168.4.1/", json=data, headers=request_header)
+def get(endpoint):
+    wifi.radio.connect("Borealis", "pico-pico")
+    pool = socketpool.SocketPool(wifi.radio)
+    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+    string = requests.get("http://192.168.4.1"+endpoint)
+    return string.text
 #MAINLOOP
 if mode == "True":
     print("HID Mode Activated")
@@ -53,10 +60,9 @@ if mode == "True":
 else:
     print("FEEDBACK Mode Activated")
     #start loop here
-    try:
-        #code to get data here
-        send() #send data
-    except adafruit_requests.OutOfRetries:
-        print("FEEDBACK Sent")
+    print(get("/log")) #get log
+    #code to get data here
+    send() #send data
+    print("FEEDBACK Sent")
     #end loop here
     while True: led.value = True #FEEDBACK FINISHED
