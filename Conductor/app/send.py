262462ced4f926f700766a525d476d2fb0eb90eb
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2024-08-18 07:12:02 +0100

    Create send.py

---
+++ Conductor/app/send.py
@@ -0,0 +1,14 @@
+presets = GET(f"/account?v=0&u={ACCOUNT()}").split("\n\n")[1].split("\n")
+bindex = 0
+while True:
+    if B2() == 0:
+        break
+    elif B1() == 0:
+        if bindex > len(presets)-1:
+            bindex = bindex + 1
+        else:
+            bindex = 0
+        DISPLAY(presets[bindex])
+    elif B0() == 0:
+        SEND({"log":presets[bindex]})
+    utime.sleep(0.5)
