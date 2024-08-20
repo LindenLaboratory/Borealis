bdd2d8d5f565845776725bb7acefc1a5d0a30544
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2024-08-20 11:15:19 +0100

    Update receive.py

--- Conductor/app/receive.py
+++ Conductor/app/receive.py
@@ -1,10 +1,11 @@
-messages = GET('/log').split("/n")
+messages = GET('/log').split("\n")[:-1]
 bindex = 0
+DISPLAY(messages[bindex])
 while True:
     if B2() == 0:
         break
     elif B1() == 0:
-        if bindex > len(messages)-1:
+        if bindex < len(messages)-1:
             bindex = bindex + 1
         else:
             bindex = 0
