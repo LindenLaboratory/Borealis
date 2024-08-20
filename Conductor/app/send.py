80527ebd1e78c770e697924a038ab1fd403cf730
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2024-08-20 11:15:41 +0100

    Update send.py

--- Conductor/app/send.py
+++ Conductor/app/send.py
@@ -1,10 +1,11 @@
 presets = GET(f"/account?v=0&u={ACCOUNT()}").split("\n\n")[1].split("\n")
 bindex = 0
+DISPLAY(presets[bindex])
 while True:
     if B2() == 0:
         break
     elif B1() == 0:
-        if bindex > len(presets)-1:
+        if bindex < len(presets)-1:
             bindex = bindex + 1
         else:
             bindex = 0
